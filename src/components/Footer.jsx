51d5da4d45fc1438bffa98a66cb610284f1ebc35
Oguzhanevin <oguzhanevinn@gmail.com>
2024-12-05 14:20:06 +0300

    güncelleme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import footerlogo from "../Assets/Iteration-2-assets/footer/logo-footer.svg";
-import icon1 from "../Assets/Iteration-2-assets/footer/icons/icon-1.png";
-import icon2 from "../Assets/Iteration-2-assets/footer/icons/icon-2.png";
-import icon3 from "../Assets/Iteration-2-assets/footer/icons/icon-3.png";
-import Instagram1 from "../Assets/Iteration-2-assets/footer/insta/li-0.png";
-import Instagram2 from "../Assets/Iteration-2-assets/footer/insta/li-1.png";
-import Instagram3 from "../Assets/Iteration-2-assets/footer/insta/li-2.png";
-import Instagram4 from "../Assets/Iteration-2-assets/footer/insta/li-3.png";
-import Instagram5 from "../Assets/Iteration-2-assets/footer/insta/li-4.png";
-import Instagram6 from "../Assets/Iteration-2-assets/footer/insta/li-5.png";
+import footerlogo from "../Assets/Iteration-2-aseets/footer/logo-footer.svg";
+import icon1 from "../Assets/Iteration-2-aseets/footer/icons/icon-1.png";
+import icon2 from "../Assets/Iteration-2-aseets/footer/icons/icon-2.png";
+import icon3 from "../Assets/Iteration-2-aseets/footer/icons/icon-3.png";
+import Instagram1 from "../Assets/Iteration-2-aseets/footer/insta/li-0.png";
+import Instagram2 from "../Assets/Iteration-2-aseets/footer/insta/li-1.png";
+import Instagram3 from "../Assets/Iteration-2-aseets/footer/insta/li-2.png";
+import Instagram4 from "../Assets/Iteration-2-aseets/footer/insta/li-3.png";
+import Instagram5 from "../Assets/Iteration-2-aseets/footer/insta/li-4.png";
+import Instagram6 from "../Assets/Iteration-2-aseets/footer/insta/li-5.png";
 
 const FooterLeft = styled.div`
   display: inline-flex;
